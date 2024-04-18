cf1c66f0d7fd2bea801c409468cac44bfa11d31d
gabriel850129 <gabriel850129@icloud.com>
2024-04-18 14:54:02 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Gabriel Estévez López on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
